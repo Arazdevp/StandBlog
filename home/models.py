3413b3959a37aa6ab30951e305f3d246ccc7ae5d
araz <araz.developer1388@gmail.com>
2025-03-02 01:58:20 +0330

    style: update Django admin panel styles with refined UI adjustments

--- home/models.py
+++ home/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Message(models.Model):
-    title = models.CharField(max_length=100)
-    text = models.TextField()
-    email = models.EmailField()
+    title = models.CharField(max_length=100, verbose_name="عنوان")
+    text = models.TextField(verbose_name="پیام")
+    email = models.EmailField(verbose_name="ایمیل")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
