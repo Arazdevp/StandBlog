364fe79d785729769c0aec75331b742d91243d05
araz <araz.developer1388@gmail.com>
2025-03-04 03:15:03 +0330

    refactor: refactor blog app views

--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,6 @@ from .models import Article, Category, Comment, Like
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.templatetags.static import static
 
-# Create your views here.
-
 
 class BaseListView(ListView):
     paginate_by = 10
@@ -27,22 +25,6 @@ class BaseListView(ListView):
         return context
 
 
-
-# def articles_list(request):
-#     if request.method == 'GET':
-#         page = request.GET.get('page')
-#
-#         paginator = Paginator(Article.objects.all(), 10)
-#
-#         try:
-#             articles = paginator.page(page)
-#         except PageNotAnInteger:
-#             articles = paginator.page(1)
-#         except EmptyPage:
-#             articles = paginator.page(paginator.num_pages)
-#
-#         return render(request, 'blog/articles_list.html', context={"articles": articles})
-
 class ArticlesListView(BaseListView):
     model = Article
     template_name = 'blog/articles_list.html'
@@ -50,36 +32,9 @@ class ArticlesListView(BaseListView):
     queryset = Article.objects.filter(published=True)
 
 
-
-# def article_details(request, slug):
-#     article = get_object_or_404(Article, slug=slug)
-#     if request.method == 'POST':
-#         body = request.POST.get('body')
-#         parent_id = request.POST.get('parent_id')
-#         Comment.objects.create(body=body, article=article, user=request.user, parent_id=parent_id)
-#
-#     context = {'article': article}
-#     if request.user.is_authenticated and (x:=request.user.likes.filter(article__slug=article.slug, user_id=request.user.id).exists()):
-#         context['like'] = True
-#     else:
-#         context['like'] = False
-#
-#     return render(request, "blog/article_detail.html", context=context)
-
-# class ArticleDetailsView(DetailView):
-#     model = Article
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         if self.request.user.is_authenticated and self.request.user.likes.filter(article__slug=self.object.slug, user_id=self.request.user.id).exists():
-#             context['like'] = True
-#         else:
-#             context['like'] = False
-#         return context
-
-
 class ArticleDetailsView(DetailView):
     model = Article
+
     # template_name = "blog/article_detail.html"
 
     def get_context_data(self, **kwargs):
@@ -110,12 +65,14 @@ class ArticleDetailsView(DetailView):
                     'body': comment.body,
                     'username': comment.user.username,
                     'created_at': comment.created_at.strftime("%Y-%m-%d %H:%M"),
-                    'profile_image_url': comment.user.profile.image.url if comment.user.profile.image else static("images/Default-Profile-picture.png"),
+                    'profile_image_url': comment.user.profile.image.url if comment.user.profile.image else static(
+                        "images/Default-Profile-picture.png"),
                     'is_reply': True if comment.parent_id else False,
                 }
             }
             return JsonResponse(data)
         return JsonResponse({'success': False})
+
 
 def category_details(request, name):
     category = get_object_or_404(Category, name=name)
@@ -139,6 +96,7 @@ def search(request):
 
     return render(request, "blog/articles_list.html", context={"articles": articles})
 
+
 def like(request, slug, pk):
     try:
         Like.objects.get(article__slug=slug, user_id=request.user.id).delete()
@@ -146,7 +104,6 @@ def like(request, slug, pk):
     except:
         Like.objects.create(article_id=pk, user_id=request.user.id)
         return JsonResponse({'response': 'Liked'})
-
 
 
 class AddArticleView(LoginRequiredMixin, CreateView):
