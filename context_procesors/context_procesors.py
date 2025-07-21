4097bbffaba4ae482678bda57bc514805609ab9a
araz <araz.developer1388@gmail.com>
2025-07-21 22:28:15 +0330

    refactor(context_procesors): add published=True filter to recent_articles queryset

--- context_procesors/context_procesors.py
+++ context_procesors/context_procesors.py
@@ -3,4 +3,4 @@ from blog.models import Category
 
 
 def recent_articles(request):
-    return {"recent_articles": Article.objects.order_by('-created')[:3], "categories": Category.objects.all()}
+    return {"recent_articles": Article.objects.filter(published=True).order_by('-created')[:3], "categories": Category.objects.all()}
